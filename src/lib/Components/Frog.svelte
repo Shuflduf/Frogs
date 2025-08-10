e0bcbf2c3e21bba66c815993b89fa3559d5af69a
Shuflduf <shuflduf@shuflduf.xyz>
2025-08-10 05:51:26 -0600

    more frog throw tweaks idk

--- src/lib/Components/Frog.svelte
+++ src/lib/Components/Frog.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { lerp, Vec2 } from '$lib';
+	import { Vec2 } from '$lib';
 	import { onMount } from 'svelte';
 
 	const DELTA = 1000 / 60;
@@ -12,10 +12,6 @@
 	let mouseVel: Vec2 = $state(new Vec2(0.0, 0.0));
 
 	$effect(() => {
-		// if (!frog) {
-		// 	return;
-		// }
-
 		frog.style.display = 'block';
 		frog.style.bottom = `${position.y}px`;
 		frog.style.left = `${position.x}px`;
@@ -30,30 +26,31 @@
 	});
 
 	function process() {
-		// position.y += delta * 100;
-		// position = new Vec2(position.x, position.y + DELTA * velocity.y);
 		if (isOnFloor()) {
 			if (velocity.y < 0.0) {
 				velocity.y = 0;
 			}
-			// const dampedX = lerp(velocity.x, 0.0, DELTA * 10);
-			// velocity = new Vec2(dampedX, velocity.y);
 			velocity = new Vec2(velocity.x * 0.9, velocity.y);
 		} else {
 			velocity.y -= DELTA * 0.005;
 		}
 
 		if (position.x < 0.0) {
-			velocity = new Vec2(Math.abs(velocity.x), velocity.y);
+			velocity = new Vec2(Math.abs(velocity.x) / 1.3, velocity.y);
 		} else if (position.x > window.innerWidth - 144) {
-			velocity = new Vec2(-Math.abs(velocity.x), velocity.y);
+			velocity = new Vec2(-Math.abs(velocity.x) / 2, velocity.y);
 		}
 
 		position = position.plus(new Vec2(DELTA * velocity.x, DELTA * velocity.y));
 		if (beingDragged) {
-			// console.log(window.)
 			position = mousePos;
 			velocity = new Vec2(0.0, 0.0);
+
+			if (position.x < 0.0) {
+				position.x = 0;
+			} else if (position.x > window.innerWidth - 144) {
+				position.x = window.innerWidth - 144;
+			}
 		}
 
 		if (isOnFloor()) {
@@ -78,11 +75,6 @@
 		const newPos = new Vec2(e.clientX - 72, window.innerHeight - e.clientY - 72);
 		mouseVel = newPos.minus(mousePos).multiply(new Vec2(1 / DELTA, 1 / DELTA));
 		mousePos = newPos;
-		// if (beingDragged) {
-		// 	position = new Vec2(e.clientX - 72, window.innerHeight - e.clientY - 72);
-		// 	velocity = new Vec2(0, 0);
-		// 	console.log(e.clientY);
-		// }
 	}
 
 	function onMouseDown(e: MouseEvent) {
