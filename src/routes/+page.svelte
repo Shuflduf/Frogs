926f98052117cf8e1c9d2cdb54d293b4a801d6d3
Shuflduf <shuflduf@shuflduf.xyz>
2025-08-10 07:23:54 -0600

    really fucking good styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,4 +9,4 @@
 <h1 class="text-center font-[Phantom_Sans] text-7xl text-[#A44A3F]">Frog Appreciation Site</h1>
 <h2 class="text-center font-[Phantom_Sans] text-2xl text-[#A44A3F] italic">i really like frogs</h2>
 
-<div class="m-8 bg-[#94A89A] p-4"><Facts /></div>
+<div class="m-8"><Facts /></div>
--- src/routes/Facts.svelte
+++ src/routes/Facts.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	const BUTTON_STYLE =
+		'absolute bottom-1/2 h-12 w-12 font-bold text-xl text-[#D4E09B] translate-y-1/2 bg-[#A44A3F] opacity-0 transition group-hover:opacity-100 font-[Phantom_Sans]';
 	let list: HTMLDivElement;
 
 	function scrollList(dir: number) {
@@ -7,20 +9,11 @@
 </script>
 
 <div class="group relative">
-	<button
-		class="absolute bottom-1/2 left-4 h-12 w-12 translate-y-1/2 bg-blue-500 opacity-0 transition group-hover:opacity-100"
-		onclick={() => scrollList(-1)}
-	>
-		&lt
-	</button>
-	<button
-		onclick={() => scrollList(1)}
-		class="absolute right-4 bottom-1/2 h-12 w-12 translate-y-1/2 bg-blue-500 opacity-0 transition group-hover:opacity-100"
-		>&gt</button
-	>
+	<button class="{BUTTON_STYLE} left-4" onclick={() => scrollList(-1)}> &lt </button>
+	<button class="{BUTTON_STYLE} right-4" onclick={() => scrollList(1)}>&gt</button>
 	<div class="flex flex-row gap-4 overflow-x-auto" bind:this={list}>
 		{#each Array(5)}
-			<div class="h-80 min-w-lg bg-red-400"></div>
+			<div class="h-80 min-w-lg bg-[#9CB380]"></div>
 		{/each}
 	</div>
 </div>
